b501e133dcdb96332e1a1af5c120b1e5d7303329
guiboli-dm <guiboli@deepmirror.com>
2023-11-29 15:48:14 +0800

    feat: single instance only

--- src_assets/client/src/main.js
+++ src_assets/client/src/main.js
@@ -7,8 +7,14 @@ const { checkIsRunning, checkIsDev, delay } = require("./main/utils");
 const { EventNamesMap } = require("./constants/constant");
 import { Worker } from "worker_threads";
 
+let mainWindow = null;
 log.initialize({ preload: true });
 app.commandLine.appendSwitch("ignore-certificate-errors");
+let isSingleInstance = app.requestSingleInstanceLock();
+log.info(`[main] isSingleInstance:`, isSingleInstance);
+if (!isSingleInstance) {
+  app.quit();
+}
 
 // Handle creating/removing shortcuts on Windows when installing/uninstalling.
 if (require("electron-squirrel-startup")) {
@@ -18,9 +24,13 @@ if (require("electron-squirrel-startup")) {
 let sunshinePid = -1;
 const createWindow = async () => {
   log.info(`[main] createWindow called`);
+  if (mainWindow) {
+    return;
+  }
+
   {
     // Create the browser window.
-    const mainWindow = new BrowserWindow({
+    mainWindow = new BrowserWindow({
       width: 800,
       height: 600,
       webPreferences: {
@@ -99,17 +109,19 @@ const createWindow = async () => {
   }
 };
 
-// This method will be called when Electron has finished
-// initialization and is ready to create browser windows.
-// Some APIs can only be used after this event occurs.
 app.on("ready", createWindow);
 
-// Quit when all windows are closed, except on macOS. There, it's common
-// for applications and their menu bar to stay active until the user quits
-// explicitly with Cmd + Q.
+app.on("second-instance", (event, argv, cwd) => {
+  if (mainWindow) {
+    if (mainWindow.isMinimized()) mainWindow.restore();
+    mainWindow.focus();
+  }
+});
+
 app.on("window-all-closed", () => {
   if (process.platform !== "darwin") {
     app.quit();
+    mainWindow = null;
   }
 });
 

9b26febec2de227ca712663832d211909a1f3d7b
guiboli-dm <guiboli@deepmirror.com>
2024-01-11 15:41:16 +0800

    feat: update

--- src_assets/client/src/main/constants/constant.js
+++ src_assets/client/src/main/constants/constant.js
@@ -2,7 +2,7 @@ const EventNamesMap = {
   SUNSHINE_KILLED: "SUNSHINE_KILLED",
   SUNSHINE_READY: "SUNSHINE_READY",
 };
-const SunshineHttpAddress = "https://0.0.0.0:47990";
+const SunshineHttpAddress = "https://0.0.0.0:47990/pin";
 
 module.exports = {
   EventNamesMap,
